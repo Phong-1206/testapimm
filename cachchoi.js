3184871ea88fd9ad84d402c61f485661af3076a9
TRAN TUAN PHONG DEVELOPER <112737379+Phong-1206@users.noreply.github.com>
2023-02-15 20:47:59 +0700

    Add files via upload

---
+++ cachchoi.js
@@ -0,0 +1,105 @@
+function cl(){
+  document.getElementById("gameName").innerHTML = "Cách Chơi Chẳn Lẻ";
+  document.getElementById("tableRewardcl").style=""
+  document.getElementById("tableRewardtx").style="display: none;"
+document.getElementById("tableRewardcl2").style="display: none;"
+document.getElementById("tableRewardg3").style="display: none;"
+document.getElementById("tableRewardt3s").style="display: none;"
+document.getElementById("tableRetbodds").style="display: none;"
+document.getElementById("tableRetbodxien").style="display: none;"
+
+document.getElementById("tableRetbodmp3").style="display: none;"
+document.getElementById("gameNoti").innerHTML = "Với nội dung : <b>C</b> hoặc <b>L</b> (nếu đuôi mã giao dịch có các số sau)"
+};
+function cl2(){
+  document.getElementById("gameName").innerHTML = "Cách Chơi Chẳn Lẻ 2";
+  document.getElementById("tableRewardcl").style="display: none;"
+document.getElementById("tableRewardtx").style="display: none;"
+document.getElementById("tableRewardcl2").style=""
+document.getElementById("tableRewardg3").style="display: none;"
+document.getElementById("tableRewardt3s").style="display: none;"
+document.getElementById("tableRetbodds").style="display: none;"
+document.getElementById("tableRetbodxien").style="display: none;"
+
+document.getElementById("tableRetbodmp3").style="display: none;"
+document.getElementById("gameNoti").innerHTML = "Với nội dung : <b>C2</b> hoặc <b>L2</b> hoặc <b>T2</b> hoặc <b>X2</b> (nếu đuôi mã giao dịch có các số sau)";
+};
+function tx(){
+  document.getElementById("gameName").innerHTML = "Cách Chơi Tài Xỉu";
+  document.getElementById("tableRewardcl").style="display: none;"
+  document.getElementById("tableRewardtx").style=""
+document.getElementById("tableRewardcl2").style="display: none;"
+document.getElementById("tableRewardg3").style="display: none;"
+document.getElementById("tableRewardt3s").style="display: none;"
+document.getElementById("tableRetbodds").style="display: none;"
+document.getElementById("tableRetbodxien").style="display: none;"
+
+document.getElementById("tableRetbodmp3").style="display: none;"
+document.getElementById("gameNoti").innerHTML = "Với nội dung : <b>T</b> hoặc <b>X</b> (nếu đuôi mã giao dịch có các số sau)";
+ // document.getElementById("cl").style="color: #fff;background-color: #6b3e9e;border-color: #6306af;"
+};
+function g3(){
+  document.getElementById("gameName").innerHTML = "Cách Chơi Gấp 3";
+  document.getElementById("tableRewardcl").style="display: none;"
+  document.getElementById("tableRewardtx").style="display: none;"
+document.getElementById("tableRewardcl2").style="display: none;"
+document.getElementById("tableRewardg3").style=""
+document.getElementById("tableRewardt3s").style="display: none;"
+document.getElementById("tableRetbodds").style="display: none;"
+document.getElementById("tableRetbodxien").style="display: none;"
+
+document.getElementById("tableRetbodmp3").style="display: none;"
+document.getElementById("gameNoti").innerHTML = "Với nội dung : <b>G3</b> (nếu đuôi mã giao dịch có các số sau)";
+};
+function t3s(){
+  document.getElementById("gameName").innerHTML = "Cách Chơi Tổng 3 Số";
+    document.getElementById("tableRewardcl").style="display: none;"
+  document.getElementById("tableRewardtx").style="display: none;"
+document.getElementById("tableRewardcl2").style="display: none;"
+document.getElementById("tableRewardg3").style="display: none;"
+document.getElementById("tableRewardt3s").style=""
+document.getElementById("tableRetbodds").style="display: none;"
+document.getElementById("tableRetbodxien").style="display: none;"
+
+document.getElementById("tableRetbodmp3").style="display: none;"
+document.getElementById("gameNoti").innerHTML = "Với nội dung : <b>S</b> (nếu tổng 3 số cuối của mã giao dịch có các số sau)";
+};
+function mp3(){
+  document.getElementById("gameName").innerHTML = "Cách Chơi Một Phần Ba";
+  document.getElementById("tableRewardcl").style="display: none;"
+  document.getElementById("tableRewardtx").style="display: none;"
+document.getElementById("tableRewardcl2").style="display: none;"
+document.getElementById("tableRewardg3").style="display: none;"
+document.getElementById("tableRewardt3s").style="display: none;"
+document.getElementById("tableRetbodds").style="display: none;"
+document.getElementById("tableRetbodxien").style="display: none;"
+document.getElementById("tableRetbodmp3").style=""
+
+document.getElementById("gameNoti").innerHTML = "Với nội dung : <b>N1</b> hoặc <b>N2</b> hoặc <b>N3</b> (nếu đuôi mã giao dịch có các số sau)";
+};
+function xien(){
+  document.getElementById("gameName").innerHTML = "Cách Chơi Xiên";
+  document.getElementById("tableRewardcl").style="display: none;"
+  document.getElementById("tableRewardtx").style="display: none;"
+document.getElementById("tableRewardcl2").style="display: none;"
+document.getElementById("tableRewardg3").style="display: none;"
+document.getElementById("tableRewardt3s").style="display: none;"
+document.getElementById("tableRetbodds").style="display: none;"
+document.getElementById("tableRetbodxien").style=""
+document.getElementById("tableRetbodmp3").style="display: none;"
+
+document.getElementById("gameNoti").innerHTML = "Với nội dung : <b>CX</b> hoặc <b>LT</b> hoặc <b>CT</b> hoặc <b>LX</b> (nếu đuôi mã giao dịch có các số sau)";
+};
+function ds(){
+  document.getElementById("gameName").innerHTML = "Cách Chơi Đoán Số";
+  document.getElementById("tableRewardcl").style="display: none;"
+  document.getElementById("tableRewardtx").style="display: none;"
+document.getElementById("tableRewardcl2").style="display: none;"
+document.getElementById("tableRewardg3").style="display: none;"
+document.getElementById("tableRewardt3s").style="display: none;"
+document.getElementById("tableRetbodxien").style="display: none;"
+
+document.getElementById("tableRetbodmp3").style="display: none;"
+document.getElementById("tableRetbodds").style=""
+document.getElementById("gameNoti").innerHTML = "Nếu mã giao dịch có số cuối trùng với 1 trong 3 số trên, bạn sẽ chiến thắng";
+};
